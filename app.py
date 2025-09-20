86f3fd2478105424c88b74c1b874dd7fd0d438e4
Balaji <71812832+bforbalaji@users.noreply.github.com>
2025-09-20 11:47:04 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask Dockerized!--> updated code'
+
+    test_str = " File changes to check PR"
+    return 'Hello from Flask Dockerized!-->' + " "+ test_str
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
